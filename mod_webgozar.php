b75c9cac0d8132cf377ffea6d427e00f68a1d0c6
GPT for Joomla <m.h.eghtedar@gmail.com>
2026-02-18 21:29:25 +0330

    Upgrade module for Joomla 6 and modernize implementation

--- mod_webgozar.php
+++ mod_webgozar.php
@@ -3,19 +3,21 @@
  * @package     Joomla.Site
  * @subpackage  mod_webgozar
  *
- * @copyright   Copyright (C) 2014 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2014 - 2026 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
-$moduleclass_sfx  = htmlspecialchars($params->get('moduleclass_sfx'));
-$type             = (string) $params->get('type');
-$newsletterLayout = (string) $params->get('newsletter', 'v');
-$code             = (int) $params->get('code');
-$showCounter      = (int) $params->get('counter', 1);
+use Joomla\CMS\Helper\ModuleHelper;
 
-if ($code)
+$moduleclass_sfx  = htmlspecialchars((string) $params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8');
+$type             = (string) $params->get('type', 'counter');
+$newsletterLayout = (string) $params->get('newsletter', 'v');
+$code             = (int) $params->get('code', 0);
+$showCounter      = (bool) $params->get('counter', 1);
+
+if ($code > 0)
 {
-	require JModuleHelper::getLayoutPath('mod_webgozar', $params->get('layout', 'default'));
+    require ModuleHelper::getLayoutPath('mod_webgozar', $params->get('layout', 'default'));
 }
